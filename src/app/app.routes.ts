acb6e653e4f541ddbf6bff4df8dea9878424e2e7
Linda M De Marco <maribeldemarco@gmail.com>
2025-05-11 14:22:43 -0300

    se sube frontend de seccion home con nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'home', component: AppComponent },
+ ];
