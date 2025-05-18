83ad6495e7636597ce500a485852d5e118d5af6e
Linda M De Marco <maribeldemarco@gmail.com>
2025-05-17 22:36:47 -0300

    se sube http client y se muestran productos en el frontend

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
 require('dotenv').config({ path: '../.env' }); // ruta para traer variables de entorno
-
 const mongoose = require('mongoose'); // Importamos Mongoose
 const express = require('express'); // Importamos Express
-
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const Producto = require('../models/products.model'); // Importamos el modelo
 
+app.use(cors()); // ✅ Esto habilita CORS para todas las solicitudes
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log(" Conectado a MongoDB"))
 .catch((err) => console.error(" Error al conectar a MongoDB:", err));
 
-app.get('/', async (req, res) => { //  
+app.get('/', async (req, res) => {  
   try {
-    const productos = await Producto.find(); //  Obtener productos 
-    console.log("Productos en la base de datos:", productos);
-    res.send('Probando servidor con productos: ' + JSON.stringify(productos)); // Enviar datos al cliente
+    const productos = await Producto.find(); // Obtener productos  
+    console.log("Productos en la base de datos:", productos);  
+    res.json(productos); // 
   } catch (error) {
-    console.error(" Error al obtener productos:", error);
+    console.error("Error al obtener productos:", error);
+    res.status(500).json({ error: "Error al obtener productos" }); 
   }
 });
 
--- src/app/bot-pedidos/bot-pedidos.component.ts
+++ src/app/bot-pedidos/bot-pedidos.component.ts
@@ -1,21 +1,31 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-
+import { ProductosService } from '../services/productos.service';
 import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
-
-/** @title Simple form field */
 @Component({
-
-   standalone: true,
-
- selector: 'bot-pedidos',
-
+  standalone: true,
+  selector: 'bot-pedidos',
   templateUrl: './bot-pedidos.component.html',
   styleUrls: ['./bot-pedidos.component.scss'],
-  imports: [MatCardModule,MatButtonModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatCardModule, MatButtonModule, CommonModule],
 })
-export class BotPedidosComponent {}
+export class BotPedidosComponent implements OnInit {
 
+  productos: any[] = [];
 
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.getProductos().subscribe({
+      next: (data) => {
+        this.productos = data;
+        console.log('Productos:', this.productos);
+      },
+      error: (err) => {
+        console.error('Error al cargar productos:', err);
+      }
+    });
+  }
+}
