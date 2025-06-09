d7069c93fb24810bd87ba17a50a1670e4eb45698
Linda M De Marco <maribeldemarco@gmail.com>
2025-06-09 00:39:04 -0300

    se ajusta componente sabores, agregando texto a las imagenes

--- src/app/sabores/sabores.component.ts
+++ src/app/sabores/sabores.component.ts
@@ -2,19 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ProductosService } from '../services/productos.service';
 import {MatCard, MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-
-
-
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-sabores',
   standalone: true,
-  imports: [MatCardModule, MatCardModule, CommonModule],
+  imports: [MatCardModule, MatCardModule, CommonModule, NgbCarouselModule],
   templateUrl: './sabores.component.html',
   styleUrl: './sabores.component.scss'
 })
 export class SaboresComponent {
   sabores: any[] = []
-  
+  mostrarFlechas = true;
+
   constructor(private productosService: ProductosService) {}
    ngOnInit(): void {
     this.productosService.getSabores().subscribe({
