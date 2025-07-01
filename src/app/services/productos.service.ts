e49bb7e191f4812511208feb6ebb58a3abe8dfd9
lindemarco <96500425+maribeldemarco@users.noreply.github.com>
2025-07-01 20:24:05 -0300

    Update productos.service.ts

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  private apiUrl = 'http://localhost:3000/'; // 
-private baseUrl = 'http://localhost:3000/dialogflow';
+  private apiUrl = environment.apiUrl;
+private baseUrl = this.apiUrl + 'dialogflow'; // https://chatbot-wb8b.onrender.com/dialogflow
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) { }
 
   getProductos(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
 
-getSabores(): Observable<any[]> { 
-    return this.http.get<any[]>(`${this.apiUrl}api/sabores`); 
-
-}
+  getSabores(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}api/sabores`);
+  }
 
   sendMessage(message: string) {
     return this.http.post<{ reply: string }>(this.baseUrl, { message });
